4d735bb4c623c849568dfa6c2d817a90743f4b0a
Adilbek2006 <adilbek200620@gmail.com>
2026-05-14 15:39:28 +0500

    updated account-service API Gateway

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	accountPb "BankingService/pb/account"
 	userPb "BankingService/pb/user"
 )
 
@@ -19,8 +20,14 @@ func main() {
 		log.Fatalf("Failed to connect to User Service: %v", err)
 	}
 	defer userConn.Close()
-
 	userClient := userPb.NewUserServiceClient(userConn)
+
+	accountConn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to Account Service: %v", err)
+	}
+	defer accountConn.Close()
+	accountClient := accountPb.NewAccountServiceClient(accountConn)
 
 	router := gin.Default()
 
@@ -29,12 +36,10 @@ func main() {
 			Name  string `json:"name"`
 			Email string `json:"email"`
 		}
-
 		if err := c.ShouldBindJSON(&reqBody); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data format"})
 			return
 		}
-
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
@@ -42,17 +47,50 @@ func main() {
 			Name:  reqBody.Name,
 			Email: reqBody.Email,
 		})
-
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating user: " + err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "ERROR: " + err.Error()})
 			return
 		}
+		c.JSON(http.StatusOK, grpcResp)
+	})
 
-		c.JSON(http.StatusOK, gin.H{
-			"message": "User successfully created",
-			"user_id": grpcResp.UserId,
-			"name":    grpcResp.Name,
+	router.POST("/accounts", func(c *gin.Context) {
+		var reqBody struct {
+			UserId   string `json:"user_id"`
+			Currency string `json:"currency"`
+		}
+		if err := c.ShouldBindJSON(&reqBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data format"})
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		grpcResp, err := accountClient.CreateAccount(ctx, &accountPb.CreateAccountRequest{
+			UserId:   reqBody.UserId,
+			Currency: reqBody.Currency,
 		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "ERROR: " + err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, grpcResp)
+	})
+
+	router.GET("/accounts/:id", func(c *gin.Context) {
+		accountID := c.Param("id")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		grpcResp, err := accountClient.GetAccountDetails(ctx, &accountPb.AccountIdRequest{
+			AccountId: accountID,
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "ERROR: " + err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, grpcResp)
 	})
 
 	log.Println("API Gateway is running on port 8080...")
