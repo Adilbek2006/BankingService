367193780249457509a7df14a9dddaaca15f1e45
Adilbek2006 <adilbek200620@gmail.com>
2026-05-14 15:02:11 +0500

    feat(user): implement postgres integration and CreateUser logic

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -9,9 +9,17 @@ import (
 
 	pb "BankingService/pb/user"
 	delivery "BankingService/user-service/internal/delivery/grpc"
+	"BankingService/user-service/internal/repository"
 )
 
 func main() {
+	db, err := repository.NewPostgresDB("localhost", "5433", "user", "password", "banking")
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	defer db.Close()
+	fmt.Println("Successfully connected to PostgreSQL!")
+
 	lis, err := net.Listen("tcp", ":50053")
 	if err != nil {
 		log.Fatalf("Failed to start listening: %v", err)
@@ -19,11 +27,12 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 
-	userHandler := delivery.NewUserHandler()
+	repo := repository.NewUserRepository(db)
+	userHandler := delivery.NewUserHandler(repo)
 
 	pb.RegisterUserServiceServer(grpcServer, userHandler)
 
-	fmt.Println(" User Service started on port: 50053...")
+	fmt.Println("User Service is running on port 50053...")
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Server Error: %v", err)
